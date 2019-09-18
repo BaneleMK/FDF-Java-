9bbd45031ef955e5b67cdea88f7e7996907522c1
Banele MKHIZE <bmkhize@c4r15s6.wethinkcode.co.za>
2019-09-18 15:28:30 +0200

    making the base files

---
+++ src/main/java/BmkhizeFDF/App.java
@@ -0,0 +1,18 @@
+package BmkhizeFDF;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
